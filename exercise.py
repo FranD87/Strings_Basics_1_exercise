5901d5449175d8c2855bb09d305b5195cf21ae90
FranD87 <fdunne007@gmail.com>
2023-01-11 14:43:05 +0100

    Commit

---
+++ exercise.py
@@ -0,0 +1,41 @@
+# Task - 1
+
+# city = "London"
+# print(city)
+
+# Task - 2
+
+# city = "berlin"
+#
+# population = "3645000"
+#
+# print(city.capitalize(), ":", population.capitalize())
+
+# Task - 3
+#
+# city = "london"
+#
+# population = 9000000
+#
+# print("City:", city.capitalize(), "(", city.isidentifier(),")")
+#
+# print("Population:", population)
+
+# task - 4
+
+# text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+#
+# print("capital:", text.find("capital"))
+
+
+# Task - 5
+
+# text = "Berlin straddles the banks of the spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau."
+#
+# print(text.split())
+
+# # Task - 6
+#
+# text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital "
+#
+# print(text.replace("capital", "capital of Germany"))
